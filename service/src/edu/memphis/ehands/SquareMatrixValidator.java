70f47d05435ef13709cd2174b6c97b042f07fb1a
Roman Birg <romanbirg@gmail.com>
2013-04-15 10:34:02 -0500

    initial commit

---
+++ service/src/edu/memphis/ehands/SquareMatrixValidator.java
@@ -0,0 +1,20 @@
+package edu.memphis.ehands;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService public class SquareMatrixValidator {
+
+    @WebMethod public boolean validateMatrix(double[][] matrix) {
+        int rows = matrix.length;
+
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i].length != rows) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
